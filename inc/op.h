fbd19010e21e655891f93037c9fae80189acbe43
vkorniie <v.korniienko91@gmail.com>
2018-10-22 11:29:48 +0300

    add main structure

--- inc/op.h
+++ inc/op.h
@@ -75,11 +75,16 @@ typedef struct					s_header
 	char						comment[COMMENT_LENGTH + 1];
 }								t_header;
 
+typedef struct					s_arg_t
+{
+	char 						arg[3];
+}								t_arg_t;
+
 typedef struct 					s_op
 {
-	char						*comand_n;
-	int							n_arguments;
-	char						*possible_arg;
+	char						*c_name;
+	int							n_arg;
+	t_arg_t						args;
 	int							op_code;
 	int							cycles;
 	char						*description;
@@ -89,28 +94,28 @@ typedef struct 					s_op
 
 static t_op						op_tab[17] =
 {
-	{"live", 1, {T_DIR}, 1, 10, "alive", 0, 4},
-	{"ld", 2, {T_DIR | T_IND, T_REG}, 2, 5, "load", 1, 4},
-	{"st", 2, {T_REG, T_IND | T_REG}, 3, 5, "store", 1, 4},
-	{"add", 3, {T_REG, T_REG, T_REG}, 4, 10, "addition", 1, 4},
-	{"sub", 3, {T_REG, T_REG, T_REG}, 5, 10, "substraction", 1, 4},
-	{"and", 3, {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR, T_REG}, 6, 6,
+	{"live", 1, {{0, 1, 0}, {0, 0, 0}, {0, 0, 0}}, 1, 10, "alive", 0, 4},
+	{"ld", 2, {{0, 1, 1}, {1, 0. 0}, {0, 0, 0}}, 2, 5, "load", 1, 4},
+	{"st", 2, {{1, 0, 0}, {1, 0, 1}, {0, 0, 0}}, 3, 5, "store", 1, 4},
+	{"add", 3, {{1, 0, 0}, {1, 0, 0}, {1, 0, 0}}, 4, 10, "addition", 1, 4},
+	{"sub", 3, {{1, 0, 0}, {1, 0, 0}, {1, 0, 0}}, 5, 10, "substraction", 1, 4},
+	{"and", 3, {{1, 1, 1}, {1, 1, 1}, {1, 0, 0}}, 6, 6,
 		"et (and  r1, r2, r3   r1&r2 -> r3", 1, 4},
-	{"or", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 7, 6,
+	{"or", 3, {{1, 1, 1}, {1, 1, 1}, {1, 0, 0}}, 7, 6,
 		"ou  (or   r1, r2, r3   r1 | r2 -> r3", 1, 4},
-	{"xor", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 8, 6,
+	{"xor", 3, {{1, 1, 1}, {1, 1, 1}, {1, 0, 0}}, 8, 6,
 		"ou (xor  r1, r2, r3   r1^r2 -> r3", 1, 4},
-	{"zjmp", 1, {T_DIR}, 9, 20, "jump if zero", 0, 2},
-	{"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 10, 25,
+	{"zjmp", 1, {{0, 1, 1}, {0, 0, 0}, {0, 0, 0}}, 9, 20, "jump if zero", 0, 2},
+	{"ldi", 3, {{1, 1, 1}, {1, 1, 0}, {1, 0, 0}}, 10, 25,
 		"load index", 1, 2},
-	{"sti", 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}, 11, 25,
+	{"sti", 3, {{1, 0, 0}, {1, 1, 1}, {1, 1, 0}}, 11, 25,
 		"store index", 1, 2},
-	{"fork", 1, {T_DIR}, 12, 800, "fork", 0, 2},
-	{"lld", 2, {T_DIR | T_IND, T_REG}, 13, 10, "long load", 1, 4},
-	{"lldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 14, 50,
+	{"fork", 1, {{0, 1, 0}, {0, 0, 0}, {0, 0, 0}}, 12, 800, "fork", 0, 2},
+	{"lld", 2, {{0, 1, 1}, {1, 0, 0}, {0, 0, 0}}, 13, 10, "long load", 1, 4},
+	{"lldi", 3, {{1, 1, 1}, {1, 1, 0}, {1, 0, 0}}, 14, 50,
 		"long load index", 1, 2},
-	{"lfork", 1, {T_DIR}, 15, 1000, "long fork", 0, 2},
-	{"aff", 1, {T_REG}, 16, 2, "aff", 1, 4},
+	{"lfork", 1, {{0, 1, 0}, {0, 0, 0}. {0, 0, 0}}, 15, 1000, "long fork", 0, 2},
+	{"aff", 1, {{1, 0, 0}, {0, 0, 0}, {0, 0, 0}}, 16, 2, "aff", 1, 4},
 	{0, 0, {0}, 0, 0, 0, 0, 0}
 };
 
