984f20c73ebd168745499b2720bcdaac6f2a4f15
vkorniie <v.korniienko91@gmail.com>
2018-11-08 11:45:27 +0200

    change almost everithing

---
+++ test.s
@@ -0,0 +1,21 @@
+#by zaz, it's not very good, but what did you expect, rainbowdash is way better
+#2013
+
+.name		"fluttershy"
+.comment	"oh, my, what a scary project"
+
+ardeftgt:
+ardefgeni:
+entry:
+
+osminog:	fork	%:coregeni
+
+privet:
+hello:
+
+	st		r1, 6
+	live	%42
+	fork	%:torpgeni
+	st		r1, 6
+	live	%42
+	fork	%:avdefgeni
